4d608dd7b6ff75a399912867f06296d405d94281
unknown <rajeshwar.s@akbartravels.in>
2018-11-12 22:30:43 +0530

    changed

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-require('dotenv').config()
+
+const config = require('./config')
 const express = require('express'),
 app = express(),
-port = process.env.PORT || 4000,
+port = process.env.PORT || config.PORT,
 mongoose = require('mongoose');
 
 bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOURL);
+mongoose.connect(config.MONGOURL);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
---
+++ config.js
@@ -0,0 +1,10 @@
+
+
+let PORT="4000"
+let MONGOURL="mongodb://me:me1234@ds159563.mlab.com:59563/battledb"
+
+module.exports={
+	PORT,
+	MONGOURL
+}
+
